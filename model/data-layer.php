6051db8f65feeb7a18d0e654ef740fa1a7066df4
Patrick Dang <pdang2@mail.greenriver.edu>
2021-02-20 01:23:53 -0500

    added data layer

---
+++ model/data-layer.php
@@ -0,0 +1,10 @@
+<?php
+/* * get survey options
+   * return array of survey options
+   * @return array
+ */
+
+function getOptions()
+{
+    return array("This midterm is easy", "I like midterms", "Today is Friday");
+}
